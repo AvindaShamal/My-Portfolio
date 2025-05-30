999e658e596fbdf23fa381058024b885d3d2f667
AvindaShamal <avindashamal@gmail.com>
2025-05-30 11:33:00 +0530

    profile

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -73,9 +73,9 @@ const Index = () => {
             
             <div className="hidden lg:block relative animate-fade-in p-4 ">
               <img 
-                src="./profile.webp" 
+                src="./profile.jpg" 
                 alt="Avinda Shamal"
-                className="rounded-lg shadow-lg w-full object-cover w-5/6 h-auto max-h-[400px] md:max-h-[500px] lg:max-h-[600px] mx-auto" 
+                className="rounded-full shadow-lg w-full object-cover" 
               />
             </div>
           </div>
